177f1e38b389c4d8bfa156b172236dcdf18ad770
kohsy11 <kohsy11@gmail.com>
2020-09-02 16:57:55 +0900

    humidity added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('postman-request')
 
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = "http://api.weatherstack.com/current?access_key=f99680efa112cfa8e0f87f96c4302bd9&query=" + latitude + "," + longitude
     request({ url, json: true }, (error, {body}) => {
@@ -8,10 +10,11 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback("Invalid location", undefined)
         } else {
+            const humidity = body.current.humidity
             const temp = body.current.temperature
             const temp_feel = body.current.feelslike
             const des = body.current.weather_descriptions[0]
-            callback(undefined, des + ". It is currently " + temp + " degrees outside and it feels like " + temp_feel + " degrees.")
+            callback(undefined, des + ". It is currently " + temp + " degrees outside and it feels like " + temp_feel + " degrees. The humidity is " + humidity + "%")
         }
     })
 }
